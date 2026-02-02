45cc48b1fcf30ccc6f57df21ee08a31910edb192
Dominic Monteiro <monteiro2silva@gmail.com>
2026-02-02 20:34:06 -0300

    fix: Update node name and error message handling in NodeTest

--- src/NodeTest.php
+++ src/NodeTest.php
@@ -10,11 +10,17 @@ class CustomNodeTest {
   public function testMethod(array $config, array $context): ?array {
 
     // do your logic here
+    // use $config['input1'] to get the input value
+    $input1 = $config['input1'] ?? null;
+
+    // $context have a array with useful information and services
+    // you can use it to access database, logger, etc.
+
 
     return [
-      'output' => OutputNodeTest::SUCCESS->value,
+      'output' => OutputNodeTest::ERROR->value,
       'data' => [
-        'message' => 'This is a test message.'
+        'message' => 'This is an error message.' . $input1
       ]
     ];
   }
